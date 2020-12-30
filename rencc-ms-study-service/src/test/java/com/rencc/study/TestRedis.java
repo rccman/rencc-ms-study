cbcd3a89dd5b4c4bc98410ce3cd9e76e8640cb19
Chaochao Ren <renchaochao@shouqiev.com>
2020-12-30 18:23:14 +0800

    Redis 测试

---
+++ rencc-ms-study-service/src/test/java/com/rencc/study/TestRedis.java
@@ -0,0 +1,89 @@
+package com.rencc.study;
+
+import com.alibaba.fastjson.JSON;
+import com.rencc.common.cache.redis.RedisService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description:
+ * @Author: renchaochao
+ * @Date: 2020/12/30 17:38
+ **/
+@Slf4j
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestRedis {
+
+    private static final String TEST_STRING_KEY = "test:String";
+    private static final String TEST_LIST_KEY = "test:List";
+    private static final String TEST_SET_KEY = "test:Set";
+    private static final String TEST_SORTED_SET_KEY = "test:sorted set";
+    private static final String TEST_HASH_KEY = "test:hash";
+
+    @Autowired
+    private RedisService redisService;
+
+
+    @Test
+    public void testRedisSetString() {
+        redisService.set(TEST_STRING_KEY, "2020-12-30 17:11:46");
+    }
+
+
+    @Test
+    public void testRedisGetString() {
+        Object value = redisService.get(TEST_STRING_KEY);
+        log.info("Redis value={}", value);
+    }
+
+    @Test
+    public void testRedisSetList() {
+
+    }
+
+    @Test
+    public void testRedisGetList() {
+
+    }
+
+    @Test
+    public void testRedisSetSet() {
+        redisService.setSet(TEST_SET_KEY, "aaa", 1000L, TimeUnit.SECONDS);
+        redisService.setSet(TEST_SET_KEY, "bbb", 1000L, TimeUnit.SECONDS);
+        redisService.setSet(TEST_SET_KEY, "ccc", 1000L, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testRedisGetSet() {
+        Set<Object> setKeys = redisService.getSetKeys(TEST_SET_KEY);
+        log.info("测试Redis Set读取", setKeys.toString());
+    }
+
+    @Test
+    public void testRedisSetSortedSet() {
+    }
+
+    @Test
+    public void testRedisGetSortedSet() {
+    }
+
+    @Test
+    public void testRedisSetHash() {
+        redisService.setHashCache(TEST_HASH_KEY, "hashKey1", "aaaa", 1000L);
+        redisService.setHashCache(TEST_HASH_KEY, "hashKey2", "aaaa", 1000L);
+        redisService.setHashCache(TEST_HASH_KEY, "hashKey3", "aaaa", 1000L);
+    }
+
+    @Test
+    public void testRedisGetHash() {
+        Object hashKey = redisService.getHashCacheAll(TEST_HASH_KEY);
+        log.info("测试Redis Hash读取value={}", JSON.toJSONString(hashKey));
+    }
+
+}
