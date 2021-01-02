a805692d5133da35b2eaa9944c54700ac4ec6da7
Chaochao Ren <renchaochao@shouqiev.com>
2021-01-02 16:19:10 +0800

    测试多线程

---
+++ rencc-ms-study-model/src/main/java/com/rencc/study/model/User.java
@@ -0,0 +1,16 @@
+package com.rencc.study.model;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: renchaochao
+ * @Date: 2021/1/2 16:14
+ **/
+@Data
+public class User {
+
+    private String name;
+
+    private Integer age;
+}
---
+++ rencc-ms-study-service/src/test/java/com/rencc/study/TestThreadMulti.java
@@ -0,0 +1,54 @@
+package com.rencc.study;
+
+import com.alibaba.fastjson.JSON;
+import com.rencc.study.model.User;
+import com.rencc.study.utils.ThreadPoolUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+/**
+ * @Description: 测试多线程
+ * @Author: renchaochao
+ * @Date: 2021/1/2 15:51
+ **/
+@Slf4j
+@SpringBootTest
+public class TestThreadMulti {
+    @Test
+    void testRunnable() throws ExecutionException, InterruptedException {
+
+        Future<?> submit = ThreadPoolUtil.threadPool.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                //抛异常
+//                int[] aa = new int[2];
+//                System.out.println(aa[3]);
+                return Thread.currentThread().getName();
+            }
+        });
+
+        Object o = submit.get();
+        System.out.println(o.toString());
+
+        User user = new User();
+
+        Future<User> userFuture = ThreadPoolUtil.threadPool.submit(new Runnable() {
+            @Override
+            public void run() {
+                //不抛异常
+//                int[] aa = new int[2];
+//                System.out.println(aa[3]);
+                user.setName(String.valueOf(Thread.currentThread().getId()));
+            }
+        }, user);
+        userFuture.get();
+        System.out.println(user.getName());
+        System.out.println(JSON.toJSONString(user));
+
+    }
+}
